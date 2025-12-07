3e6c01149a1f1ff57ae28aa6c8a726f84c80c716
Jon Hogg <hogg.jon+github@gmail.com>
2025-12-07 12:11:46 +0000

    Updated Version numbers

--- Core/SharedFuncs.lua
+++ Core/SharedFuncs.lua
@@ -4,14 +4,14 @@ local _G = _G
 
 local buildInfo = {
 	-- wow_classic_beta    = {"MOP Classic Beta",          "5.5.0",  62071},
-	-- wow_beta            = {"The War Within Beta",       "11.0.2", 56313},
-	wow_classic_ptr     = {"PTR (MoP Classic)",			"5.5.0",  62232},
-	wow_classic_era_ptr = {"PTR (Classic Era)",         "1.15.7", 60141},
-	wow_ptr             = {"PTR (TWW 11.1.7)",          "11.1.7", 61967},
-	wow_ptr_x           = {"PTR (TWW 11.2.0)",          "11.2.0", 62417},
-	wow_classic_era     = {"World of Warcraft Classic", "1.15.7", 61582},
-	wow_classic         = {"Mists of Panderia Classic", "5.5.0",  62258},
-	wow                 = {"World of Warcraft",         "11.2.0", 62417},
+	wow_beta            = {"Midnight Beta",             "12.0.0", 64741},
+	wow_classic_ptr     = {"PTR (MoP Classic)",			"5.5.3",  64530},
+	wow_classic_era_ptr = {"PTR (Classic Era)",         "1.15.8", 64616},
+	wow_ptr             = {"PTR (TWW 11.2.5)",          "11.2.5", 63660},
+	wow_ptr_x           = {"PTR (TWW 11.2.7)",          "11.2.7", 64587},
+	wow_classic_era     = {"World of Warcraft Classic", "1.15.8", 64272},
+	wow_classic         = {"Mists of Panderia Classic", "5.5.2",  64481},
+	wow                 = {"World of Warcraft",         "11.2.7", 64743},
 	curr                = {"curr", _G.GetBuildInfo()},
 }
 
