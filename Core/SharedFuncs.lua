5a1c96f41318fb500d3c32ddcc961dad78c2ef72
Jon Hogg <hogg.jon+github@gmail.com>
2026-08-11 17:18:19 +0100

    Updated Version numbers

--- Core/SharedFuncs.lua
+++ Core/SharedFuncs.lua
@@ -3,16 +3,16 @@ local aName, aObj = ...
 local _G = _G
 
 local buildInfo = {
-	wow_classic_beta    	= {"MOP Classic Beta",          "5.5.0",  62071},
-	wow_beta            	= {"Midnight Beta",             "12.0.1", 66220},
-	wow_classic_ptr     	= {"PTR (MoP Classic)",			"5.5.4",  67440},
-	wow_classic_era_ptr 	= {"PTR (Classic Era)",         "2.5.5",  67323},
-	wow_ptr             	= {"PTR (Midnight 12.0.5)",		"12.0.5", 67235},
-	wow_ptr_x           	= {"PTR (Midnight 12.0.7",      "12.0.7", 67360},
-	wow_classic_anniversary	= {"BC Anniversary", 			"2.5.5",  67157},
-	wow_classic_era     	= {"World of Warcraft Classic", "1.15.8", 67156},
-	wow_classic         	= {"Mists of Panderia Classic", "5.5.3",  67158},
-	wow                 	= {"World of Warcraft",         "12.0.5", 67403},
+	-- wow_classic_beta    	= {"MOP Classic Beta",          	"5.5.0",  62071},
+	-- wow_beta            	= {"Midnight Beta",             	"12.0.1", 66220},
+	wow_classic_ptr     	= {"PTR (MoP Classic)",				"5.5.4",  67849},
+	wow_classic_era_ptr 	= {"PTR (Classic Era)",         	"2.5.6",  69110},
+	wow_ptr             	= {"PTR (Midnight 12.1.0)",			"12.1.0", 69587},
+	wow_ptr_x           	= {"PTR (Midnight 12.1.5",      	"12.1.5", 69594},
+	wow_classic_anniversary	= {"Burning Crusade Anniversary",	"2.5.6",  69546},
+	wow_classic_era     	= {"World of Warcraft Classic", 	"1.15.9", 69547},
+	wow_classic         	= {"Mists of Panderia Classic", 	"5.5.4",  69585},
+	wow                 	= {"World of Warcraft",         	"12.1.0", 69814},
 	curr                	= {"curr", _G.GetBuildInfo()},
 }
 
